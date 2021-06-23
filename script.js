3eca4bdde0afa0ee067986d4a5cae732f56afcbd
Ganeshscr2511 <82515080+Ganeshscr2511@users.noreply.github.com>
2021-06-23 13:29:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
-fetch('https://api.github.com/search/repositories?q=stars:150000..300000', {
-  method: "GET"
-})
-  .then((data) => data.json())
-.then((res) => console.log(res))
-  .then((users) => {
-    const allUsers = document.querySelector(".userRepositoryNames");
-    users.forEach((user) => {
-      // user container
-      const userConatiner =  document.createElement('div');
-      userConatiner.setAttribute('class','div');
+// fetch('https://api.github.com/search/repositories?q=stars:150000..300000', {
+//   method: "GET"
+// })
+//   .then((data) => data.json())
+// .then((res) => console.log(res))
+//   .then((users) => {
+//     const allUsers = document.querySelector(".userRepositoryNames");
+//     users.forEach((user) => {
+//       // user container
+//       const userConatiner =  document.createElement('div');
+//       userConatiner.setAttribute('class','div');
       
-      allUsers.innerHTML =`<a href='${user.items.html_url}'> ${user.full_name} </a>`;
+//       allUsers.innerHTML =`<a href='${user.items.html_url}'> ${user.full_name} </a>`;
      
-       allUsers.append(userConatiner);
+//        allUsers.append(userConatiner);
       
+//     });
+//   });
+
+
+
+
+function onsearch(event){
+    if(event.keyCode === 13){
+      
+    const searchText=document.getElementById("search-box").value;
+    const originalText=searchText.split(" ").join(" ");
+    let url='https://api.github.com/orgs/`${originalText}`/repos';
+  
+  fetch(url , {
+    method: "GET"
+  })
+    .then((data) => data.json())
+  
+    .then((users) => {
+      const allUsers = document.querySelector(".userRepositoryNames");
+      users.forEach((user) => {
+        // user container
+        const userConatiner =  document.createElement('div');
+        userConatiner.setAttribute('class','div');
+        
+        userConatiner.innerHTML = `
+        <img src='${user.avatar_url}' alt='profile-Img'>
+        `+`<br>`+ `
+        <a href="${user.repos_url}">${user.name}</a>`
+        ;
+       
+         allUsers.append(userConatiner);
+        
+      });
     });
-  });
-
-
-function searchClick(){
-    let searchBar=document.getElementById("search-bar");
-    searchBar.style.width="500px";
-    searchBar.style.background="grey";
-    searchBar.style.color="#000";
-    searchBar.style.border="1px solid blue";
-
-}
-
-async function searchfunction(event){
-    let searchText=document.getElementById("search-bar").value;
-    
-    
-    if (event.keyCode === 13) {
-        const url="https://api.github.com"
-        const response=await fetch(url);
-        const result=response.json();
-
-        console.log(result);
-    
     }
-
-}
-
-
-function explore(){
-    fetch('https://api.github.com/orgs/ORG/repos', {
-  method: "GET"
-})
- .then((data) => data.json())
- .then((data1)=> {
-    const explore=document.querySelector(".explore-repository");
-
-    explore.innerHTML=`<h1> ${data1} </h1>`;
- 
-});
-}
+    
+  }
